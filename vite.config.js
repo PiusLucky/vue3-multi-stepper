d2754e73811466ca51644e2ab0deeea164fb5cd5
PiusLucky <luckypius50@gmail.com>
2023-07-08 14:09:06 +0100

    chore: update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath } from "url";
-import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
