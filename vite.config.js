e93d0634605326a3b3529657fd4e028e1c51bc7c
PiusLucky <luckypius50@gmail.com>
2023-07-08 14:14:15 +0100

    new: setup npm publishing

--- vite.config.js
+++ vite.config.js
@@ -4,27 +4,27 @@ import vue from "@vitejs/plugin-vue";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  base: "",
+  base: "/vue3-multi-stepper/",
   plugins: [vue(), cssInjectedByJsPlugin()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url).href),
     },
   },
-  // build: {
-  //   lib: {
-  //     entry: resolve(__dirname, "lib/main.js"),
-  //     name: "vue3-multi-stepper",
-  //     fileName: "vue3-multi-stepper",
-  //   },
-  //   rollupOptions: {
-  //     input: 'src/main.js',
-  //     external: ["vue"],
-  //     output: {
-  //       globals: {
-  //         vue: "Vue",
-  //       },
-  //     },
-  //   },
-  // },
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.js"),
+      name: "vue3-multi-stepper",
+      fileName: "vue3-multi-stepper",
+    },
+    rollupOptions: {
+      input: "src/main.js",
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
 });
